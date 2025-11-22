de5b7109320849c5ed94d9aa735571d5ad5affe3
panasovskyi <ie.panasovskyi@gmail.com>
2025-11-22 16:56:30 +0000

    cors

--- src/index.js
+++ src/index.js
@@ -2,34 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { recipeRouter } from "./routers/recipe.router.js";
-import { sequelize } from "./db.js"; // підключаємо базу
 
 dotenv.config();
 const PORT = process.env.PORT || 1111;
 
 const app = express();
 
-// Middleware
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*", // або конкретно URL фронтенду на GitHub Pages
-  })
-);
+app.use(cors());
 
-// Роутер
 app.use("/recipes", recipeRouter);
 
-// Перевірка підключення до бази перед запуском сервера
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("DB connected successfully");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Unable to connect to DB:", err);
-  });
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
